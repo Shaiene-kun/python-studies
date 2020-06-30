44ad449f4c37356529462ddb0b55a3ce4385c81c
Shaiene-kun <rafaelshaiene115@gmail.com>
2020-06-30 17:20:17 -0300

    Added study of collections

---
+++ Starting/listsTuplesSets.py
@@ -0,0 +1,10 @@
+fruitList = ["apple", "banana", "cherry"]
+fruitTuple = ("apple", "banana", "cherry")
+fruitSet = {"apple", "banana", "cherry"}
+
+for x in fruitSet:
+    print(x) 
+if "orange" in fruitSet:
+    print(True)
+else:
+    print(False)
