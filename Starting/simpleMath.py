03772df8cc136b93e413aac72baef2c81ab7fc8e
Shaiene-kun <rafaelshaiene115@gmail.com>
2020-06-30 00:39:43 -0300

    Simple math and printing usage

---
+++ Starting/simpleMath.py
@@ -0,0 +1,15 @@
+a = 1
+b = 2
+sum = a + b
+sub = a - b
+mult =  a * b
+div = a / b
+remains = a % b
+print ("a = " + str(a) + ", b = " + str(b) )
+print ("Sum: " + str(sum))
+print ("Sub: " + str(sub))
+print ("Mult: " + str(mult))
+print ("Div: " + str(div))
+print ("Whole Div: " + str( int(div) ) )
+print ("Remains: " + str(remains) )
+
