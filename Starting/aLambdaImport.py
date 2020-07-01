ebb27a5e730cf05f42c7226d84d27e0a57f48352
Shaiene-kun <rafaelshaiene115@gmail.com>
2020-06-30 22:03:42 -0300

    Added Lambda and import tests

---
+++ Starting/aLambdaImport.py
@@ -0,0 +1,5 @@
+import bLambdaImport
+a = lambda : print("a")
+if __name__ == "__main__":
+    print(a())
+print("Out of if: a")
---
+++ Starting/bLambdaImport.py
@@ -0,0 +1,5 @@
+import aLambdaImport
+b = lambda : print("b")
+if __name__ == "__main__":
+    print(b())
+print("Out of if: b")
