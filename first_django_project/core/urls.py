e1a7bb4309443aa52b8b3bbc4bf1b6dfb1ba9bf6
Shaiene <shaiene-kun@pop-os.localdomain>
2020-07-19 15:36:38 -0300

    Added posts page, lists all posts

--- first_django_project/core/urls.py
+++ first_django_project/core/urls.py
@@ -1,5 +1,11 @@
 from django.urls import path 
 from . import views
+from .views import HomeView
+
+app_name = 'core'
+
 urlpatterns = [
-    path('', views.home, name="home" ) 
+    #path('', views.home, name="home" ) 
+    path('', HomeView.as_view(), name = "home"),
+
 ]
--- first_django_project/core/views.py
+++ first_django_project/core/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import Post
+
+# Class views
+class HomeView(ListView):
+    model = Post
+    template_name = "home.html"
 
 # Create your views here.
-def home(request):
-    return render(request, 'home.html', {})
+
+# Function Views
+# def home(request):
+#     return render(request,'home.html', {})
